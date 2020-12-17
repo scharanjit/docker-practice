bd17e8d824c93784ecaaa46a830e1ce808ccee33
Charanjit Singh <charanjit.singh@veranahealth.com>
2020-12-17 18:19:36 +0530

    Docker basics

---
+++ src/main/resources/app.js
@@ -0,0 +1,67 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+const { MongoClient } = require("mongodb");
+
+const uri = `mongodb://${process.env.MONGO_URI}:${process.env.MONGO_PORT}`;
+
+const client = new MongoClient(uri);
+
+
+async function runMongo() {
+  let isConnected = false;
+  try {
+    await client.connect();
+    await client.db("admin").command({ping: 1});
+    isConnected = true;
+    console.log("connected to mongo")
+  } catch (e) {
+    console.log("Not connected..something went wrong", e);
+  } finally {
+    await client.close();
+    return isConnected;
+  }
+}
+
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.get('/test',async (req,res,next) =>{
+  const result = await runMongo();
+  res.send(`Mongo DB is ${result ? 'connected' : 'not connected'}`);
+})
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
